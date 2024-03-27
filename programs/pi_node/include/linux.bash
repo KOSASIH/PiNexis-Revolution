c11927b071a3328466619ad7e23020ed9db9cbea
KOSASIH <kosasihg88@gmail.com>
2024-03-27 12:01:48 +0700

    Create linux.bash

---
+++ programs/pi_node/include/linux.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "#ifndef MY_HEADER_FILE_H" > include/my_header_file.h
+echo "#define MY_HEADER_FILE_H" >> include/my_header_file.h
+echo "" >> include/my_header_file.h
+echo "#endif /* MY_HEADER_FILE_H */" >> include/my_header_file.h
